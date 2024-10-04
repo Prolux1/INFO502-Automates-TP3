69ed361b987b1be6aa9a67a77b861c117af0baba
Prolux1 <cyrilberruyer@outlook.com>
2024-10-04 16:15:39 +0200

    Fin séanec tp3

--- src/Etat.java
+++ src/Etat.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Etat {
+    private Etat arrivee;
+    private int init_etat;
+    private boolean is_initial;
+    private boolean is_final;
+
+    public Etat(int init_etat) {
+        this(init_etat,false,false);
+    }
+
+    public Etat(int init_etat, boolean is_initial) {
+        this(init_etat, is_initial, false);
+    }
+
+    public Etat(int init_etat, boolean is_initial, boolean is_final) {
+        this.init_etat = init_etat;
+        this.is_initial = is_initial;
+        this.is_final = is_final;
+    }
+
+    public boolean getIsInitial() {
+        return is_initial;
+    }
+    public boolean getIsFinal() {
+        return is_final;
+    }
+
+    public Etat transition(Character lettre, ArrayList<HashMap<Character,Etat>> transitions) {
+        return transitions.get(init_etat).get(lettre);
+    }
+
+    public boolean hasTransition(Character lettre, ArrayList<HashMap<Character,Etat>> transitions) {
+        return transitions.get(init_etat).get(lettre) != null;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,36 @@
-
-
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        AutomateDet aut = new AutomateDet(new Alphabet("a", "b", "c"), new EnsembleFiniEtats(new Etat()));
+        // Question 3
+
+        ArrayList<Etat> etats = new ArrayList<>();
+        etats.add(new Etat(0, true));
+        etats.add(new Etat(1));
+        etats.add(new Etat(2, false, true));
+
+        ArrayList<HashMap<Character,Etat>> transitions = new ArrayList<>();
+
+        HashMap<Character, Etat> t1 = new HashMap<>();
+        t1.put('b', etats.get(1));
+        transitions.add(t1);
+
+        HashMap<Character, Etat> t2 = new HashMap<>();
+        t2.put('a', etats.get(1));
+        t2.put('b', etats.get(2));
+        transitions.add(t2);
+
+        HashMap<Character, Etat> t3 = new HashMap<>();
+        t3.put('b', etats.get(2));
+        t3.put('c', etats.get(1));
+        transitions.add(t3);
+
+        AutomateDet aut = new AutomateDet(etats, transitions);
+        System.out.println("mot \"bbb\": " + aut.reconnait("bbb"));
+        System.out.println("mot \"bab\": " + aut.reconnait("bab"));
+        System.out.println("mot \"babcb\": " + aut.reconnait("babcb"));
+        System.out.println("mot \"ba\": " + aut.reconnait("ba"));
+        System.out.println("mot \"baa\": " + aut.reconnait("baa"));
     }
 }
